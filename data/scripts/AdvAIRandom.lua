207289b6436f27b5dc78a51ef5d697de14163fe1
Hedde Bosman <sgorpi@gmail.com>
2004-01-01 19:39:21 +0000

    AdvAIRandom nog wat verbeterd. zou nou goed moeten lopen, en checked ook welke kant ie uit kan lopen om bij zn doel te komen bugfeedback plz

--- data/scripts/AdvAIRandom.lua
+++ data/scripts/AdvAIRandom.lua
@@ -36,12 +36,15 @@ AdvAIRandom = Controller:subclass
 	-- 
 	notifyBumpInto = function(self, obj)
 		-- Pause for some time and choose another direction
-		self.waitTime = math.random(100) + 10
+		if (self.pawn.bAttacking == false) then
+			self.waitTime = math.random(100) + 10
+		end
 	end;
 	notifyWalkFinished = function(self)
-                if (self.distanceToWalk <= 0) then
+                if (self.distanceToWalk <= 0 and self.pawn.bAttacking == false) then
 		        -- Reached his goal, pause and choose new goal.
-			self.waitTime = math.random(100) + 10
+			-- korte wachttijd, anders staat ie na elke tile even (te lang) stil
+			self.waitTime = math.random(4) + 1
 		else
 		        -- Walking to goal, keep walking.
 		        self.distanceToWalk = self.distanceToWalk - 1
@@ -61,12 +64,26 @@ AdvAIRandom = Controller:subclass
 		end;
 	end;
 	notifyTakeDamage = function(self, damage, instigator, damageType, momentum, location)
-		m_message("Rat: damaged. nature: " .. self.nature)
 		if (instigator and instigator:instanceOf(Player)) then
 			self:setTargetWithNature(self.nature, instigator);
 		end;
 	end;
 	
+	-- check if we can go in 'dir'-ection ... if not, choose any other, but not 'notdir'
+	goingDirection = function(self, dir, notdir)
+		local dirs = self.pawn:freeTilesAround()
+		for v in dirs do
+		    if (v == dir) then
+			return dir
+		    end;
+		end;
+		for v in dirs do
+		    if (v ~= notdir) then
+			return v;
+		    end;
+		end;
+		return nil
+	end;
 
 	-- timebom
 	tick = function(self)
@@ -74,60 +91,61 @@ AdvAIRandom = Controller:subclass
 
 		if (self.pawn.charging > 0) then self.pawn.charging = self.pawn.charging - 1 end
 		
-		if (self.waitTime > 0) then self.waitTime = self.waitTime - 1; end;
+		if (self.waitTime > 0) then 
+			self.waitTime = self.waitTime - 1;
 
-		if (self.nature == MOODY) then
-			-- this one is shifting moods... do new mood, and state how long it has that mood
-			self.moodTime = self.moodTime -1
-			if (self.moodTime <= 0) then
-				self.target = nil
-				self.scaring = nil
-				self.moodTime = 300 + math.random(1000)
-				self.nature_tmp = math.random(3)
-			end;
-		end;
-		
-		if (self.waitTime <= 0 and self.pawn.bAttacking == false) then
-			-- Check for targets and their distances
-			if (self.target) then
-				playerDist = self.pawn:distanceTo(self.target)
-			elseif (self.scaring) then
-				playerDist = self.pawn:distanceTo(self.scaring)
-			else
-				playerDist = 100
+			if (self.nature == MOODY) then
+				-- this one is shifting moods... do new mood, and state how long it has that mood
+				self.moodTime = self.moodTime -1
+				if (self.moodTime <= 0) then
+					self.target = nil
+					self.scaring = nil
+					self.moodTime = 500 + math.random(1000)
+					self.nature_tmp = math.random(3)
+				end;
 			end;
 			
-			if (playerDist == 1 and self.target) then
-				self.pawn.dir = self.pawn:directionTo(self.target)
-				self.pawn:attack()
-				self.waitTime = 100
-			end;
+			if (self.waitTime <= 0 and self.pawn.bAttacking == false) then
+				-- Check for targets and their distances
+				if (self.target) then
+					playerDist = self.pawn:distanceTo(self.target)
+				elseif (self.scaring) then
+					playerDist = self.pawn:distanceTo(self.scaring)
+				else
+					playerDist = 100
+				end;
+			
+				if (playerDist == 1 and self.target) then
+					self.pawn.dir = self:goingDirection(self.pawn:directionTo(self.target), nil)
+					self.pawn:attack()
+					self.waitTime = self.pawn.attackTime + self.pawn.chargeTime + 10
+				end;
 
-			self.distanceToWalk = 1
+				self.distanceToWalk = 0
 
-			if (self.target and playerDist < 5) then
-				-- hot persuit mode
-				self.pawn.dir = self.pawn:directionTo(self.target);
-			elseif (self.scaring and playerDist < 15) then
-				-- scared; will run in opposit direction
-				local tmpdir = self.pawn:directionTo(self.scaring)
-				self.pawn.dir = self:reverseDirection(tmpdir);
-			else
-				-- walk around
-				self.scaring = nil;
-				self.target = nil;
-				self.pawn.dir = self.pawn:randomFreeTileAround()
-			end;
-
-			self.pawn:walk(self.pawn.dir)
-		end
+				if (self.target and playerDist < 5) then
+					-- hot persuit mode
+					self.pawn.dir = self:goingDirection(self.pawn:directionTo(self.target), nil);
+				elseif (self.scaring and playerDist < 15) then
+					-- scared; will run in opposit direction
+					local tmpdir = self.pawn:directionTo(self.scaring)
+					self.pawn.dir = self:goingDirection(self:reverseDirection(tmpdir), tmpdir);
+				else
+					-- walk around
+					self.scaring = nil;
+					self.target = nil;
+					self.pawn.dir = self.pawn:randomFreeTileAround()
+				end;
+				self.pawn:walk(self.pawn.dir)
+			end
+		end;
 	end;
 
 	defaultproperties = {
 		tick_time = 0,
-		waitTime = 0,
+		waitTime = 20,
 		distanceToWalk = 0,
-		nature = MOODY,
+		nature = AGGRESSIVE,
 		nature_tmp = NEUTRAL,
 		moodTime = 500,
 		target = nil,
